7369606f2c4844cf738d9959b6a328ef8554236e
slguan <slguan@taosdata.com>
2019-12-05 22:30:05 +0800

    version.c

--- src/util/src/version.c
+++ src/util/src/version.c
@@ -1,4 +1,4 @@
-char version[64] = "2.0.0.0";
-char compatible_version[64] = "1.6.1.0";
-char gitinfo[128] = "0791fb439df9504003d33f83ce49779411ec7e35";
-char buildinfo[512] = "Built by ubuntu at 2019-11-18 09:37";
+char version[64] = "2.0.0.1";
+char compatible_version[64] = "2.0.0.1";
+char gitinfo[128] = "5fc1771b0cc41cdba2fc1217a012065564d473c0";
+char buildinfo[512] = "Built by ubuntu at 2019-12-05 22:27";
