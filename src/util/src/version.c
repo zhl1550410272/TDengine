d5de48cb1b88d493a870ac0f0371339479067bfd
plum-lihui <2849823933@qq.com>
2020-06-11 10:38:36 +0800

    [update branch version info for enterprise]

--- src/util/src/version.c
+++ src/util/src/version.c
@@ -1,7 +1,7 @@
 char version[64] = "1.6.6.0";
 char compatible_version[64] = "1.6.0.0";
-char gitinfo[128] = "71fbec59774636d13641a6097a1792f6a3bfeee0";
-char gitinfoOfInternal[128] = "";
-char buildinfo[512] = "Built by root at 2020-06-11 10:36";
+char gitinfo[128] = "e69e95a4a9c771140bff568217874e66bf1f6c57";
+char gitinfoOfInternal[128] = "a0339e1311c1f626de8c7d8efa51ef026c98ce86";
+char buildinfo[512] = "Built by root at 2020-06-11 10:38";
 
-void libtaos_edge_1_6_6_0_Linux_x64() {};
+void libtaos_1_6_6_0_Linux_x64() {};
